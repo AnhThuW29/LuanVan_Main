44783a975fb8a3180af3e8f015fb7e065cea7fdb
AnhThu <yinctvn@gmail.com>
2022-12-12 18:16:49 +0700

    push select

---
+++ src/api/urlGetDataAPI.js
@@ -0,0 +1,9 @@
+// IP MAY
+const ip = "192.168.88.109";
+
+const portServer = "9000";
+
+const URL_IMAGES = "http://" + ip + ":" + portServer + "/publics/images/";
+const URL_REQUESTAXIOS = "http://" + ip + ":" + portServer + "/v1";
+
+export { URL_IMAGES, URL_REQUESTAXIOS };
